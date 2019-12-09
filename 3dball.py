467edde8b4133e5d3c5dba08ab9635919ab28ddf
Jonatan Holmquist <jh19007@harryda.se>
2019-12-09 09:53:24 +0100

    3d ball is nice

--- 3dball.py
+++ 3dball.py
@@ -1,21 +1,8 @@
-maxvalue = 300
-coordinates = [-300,-300,-300]
-speeds = [30,20,10]
+maxvalue = 100
+coordinates = [12,-34,15]
+speeds = [12,9,13]
 import turtle
 import time
-sscreen = turtle.Screen()
-ball = turtle.Turtle()
-wall = turtle.Turtle()
-ball.ht()
-ball.speed(0)
-wall.ht()
-wall.speed(0)
-wall.penup()
-wall.goto(maxvalue,maxvalue)
-wall.pendown()
-for i in range(0,4):
-    wall.right(90)
-    wall.forward(2*maxvalue)
 def move(dimension):
     global coordinates
     global speeds
@@ -26,12 +13,33 @@ def move(dimension):
     if coordinates[dimension] > maxvalue:
         coordinates[dimension] = maxvalue - (coordinates[dimension]-maxvalue)
         speeds[dimension] = speeds[dimension] * -1
+sscreen = turtle.Screen()
+ball = turtle.Turtle()
+wall = turtle.Turtle()
+ball.ht()
+ball.speed(0)
+wall.ht()
+wall.speed(0)
+wall.penup()
+wall.goto(maxvalue+10,maxvalue+10)
+wall.pendown()
+for i in range(0,4):
+    wall.right(90)
+    wall.forward((2*maxvalue)+20)
+wall.goto((maxvalue*4)+20,maxvalue*4+20)
+for i in range(0,4):
+    wall.right(90)
+    wall.forward((8*maxvalue)+40)
+    wall.right(135)
+    wall.fd((((3*maxvalue+10)**2)+((maxvalue*3+10)**2))**(1/2))
+    wall.bk((((3*maxvalue+10)**2)+((maxvalue*3+10)**2))**(1/2))
+    wall.left(135)
 while True:
     ball.clear()
     ball.penup()
-    ball.setposition(coordinates[0], coordinates[1])
+    ballsize = (coordinates[2] + maxvalue)+ 10
+    ball.setposition(coordinates[0], coordinates[1]-(ballsize/2))
     ball.pendown()
-    ballsize = (coordinates[2]+ maxvalue)*0.1 + 10
     ball.circle(ballsize)
     move(0)
     move(1)
---
+++ test turtle.py
@@ -0,0 +1,10 @@
+import turtle
+b = turtle.Screen()
+a = turtle.Turtle()
+a.right(90)
+a.fd(50)
+a.left(90)
+a.circle(25)
+a.ht()
+while True:
+    pass
